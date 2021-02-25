0872a5a53d72c85e35eced155355fc52faa98da9
Yan Garcia <yangarciavaz@gmail.com>
2021-02-25 20:21:17 -0300

    another context created and more

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/yangvaz.png" alt="Yan Garcia" />
@@ -8,7 +12,8 @@ export function Profile() {
         <strong> Yan Garcia </strong>
         <p>
           <img src="icons/level.svg" alt="level" />
-           Level 1 </p>
+           Level {level}
+        </p>
       </div>
     </div>
   );
